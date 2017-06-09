c59f396aaf869664949333fa750ceff4fee3b96c
Kristofer Hanes <kristoferhanes@gmail.com>
2017-06-08 18:24:51 -0700

    Initial Commit

---
+++ Prelude/Prelude.h
@@ -0,0 +1,19 @@
+//
+//  Prelude.h
+//  Prelude
+//
+//  Created by Kristofer Hanes on 6/8/17.
+//  Copyright © 2017 Kristofer Hanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Prelude.
+FOUNDATION_EXPORT double PreludeVersionNumber;
+
+//! Project version string for Prelude.
+FOUNDATION_EXPORT const unsigned char PreludeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prelude/PublicHeader.h>
+
+
