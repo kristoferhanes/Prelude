86813818e9196926b7bc1ede096f8796c0816311
Kristofer Hanes <kristoferhanes@gmail.com>
2017-11-03 14:53:03 -0700

    Made mapJson partial to trap on malformed JSON

--- Prelude/Json.swift
+++ Prelude/Json.swift
@@ -8,58 +8,57 @@
 
 import Foundation
 
-enum Json {
+public enum Json {
   case null
   case text(String)
   case number(Double)
   case boolean(Bool)
-  case object([String: Json])
+  case object([String:Json])
   case list([Json])
 }
 
-struct JsonInitError: Error { }
-
-extension Json {
+public extension Json {
   
   init(data: Data) throws {
     
-    func mapJson(from object: Any) -> Json? {
+    func mapJson(from object: Any) -> Json {
       switch object {
       case _ as NSNull: return .null
       case let string as String: return .text(string)
       case let number as NSNumber:
         let type = String(utf8String: number.objCType)
         return type == "c" ? .boolean(number.boolValue) : .number(number.doubleValue)
-      case let dictionary as [String:Any]: return dictionary.traverse(mapJson).map(Json.object)
-      case let array as [Any]: return array.traverse(mapJson).map(Json.list)
-      default: return nil
+      case let dictionary as [String:Any]: return .object(dictionary.mapValues(mapJson))
+      case let array as [Any]: return .list(array.map(mapJson))
+      default: fatalError("Malformed JSON")
       }
     }
     
     let object = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-    guard let mapped = mapJson(from: object)
-      else { throw JsonInitError() }
-    self = mapped
-  }
-  
-  var isNull: Bool {
-    guard case .null = self else { return false }
-    return true
+    self = mapJson(from: object)
   }
   
 }
 
-typealias JsonReader<Read> = Reader<Json, Read>
-
-struct JsonReaderError: Error {
-  let message: String
+public struct JsonReaderError: Error {
+  let msg: String
   
   init(_ message: String) {
-    self.message = message
+    msg = message
+  }
+  
+  init(expected: String, in json: Json) {
+    msg = "Expected \(expected) but got \(json)"
   }
 }
 
-extension Reader where Source == Json {
+public typealias JsonReader<Read> = Reader<Json, Read>
+
+public extension Reader where Source == Json {
+  
+  func reading(from data: Data) throws -> Read {
+    return try reading(from: Json(data: data))
+  }
   
   var optional: JsonReader<Read?> {
     return JsonReader<Read?> { json in try? self.reading(from: json) }
@@ -67,32 +66,38 @@ extension Reader where Source == Json {
   
   var nullable: JsonReader<Read?> {
     return JsonReader<Read?> { json in
-      if json.isNull { return nil }
+      if case .null = json { return nil }
       return try self.reading(from: json)
     }
   }
   
 }
 
-extension Json {
+public extension Json {
   
   static var string: JsonReader<String> {
     return JsonReader { json in
-      guard case let .text(str) = json else { throw JsonReaderError("expected string") }
+      guard case let .text(str) = json else { throw JsonReaderError(expected: "string", in: json) }
       return str
+    }
+  }
+  
+  static var url: JsonReader<URL> {
+    return string.map { str in
+      guard let url = URL(string: str) else { throw JsonReaderError("\"\(str)\" is not a valid url") }
+      return url
     }
   }
   
   static var double: JsonReader<Double> {
     return JsonReader { json in
-      guard case let .number(num) = json else { throw JsonReaderError("expected double") }
+      guard case let .number(num) = json else { throw JsonReaderError(expected: "number", in: json) }
       return num
     }
   }
   
   static var int: JsonReader<Int> {
-    return JsonReader { json in
-      guard case let .number(num) = json else { throw JsonReaderError("expected integer") }
+    return double.map { num in
       guard let i = Int(exactly: num) else { throw JsonReaderError("\(num) is not an integer") }
       return i
     }
@@ -100,23 +105,35 @@ extension Json {
   
   static var bool: JsonReader<Bool> {
     return JsonReader { json in
-      guard case let .boolean(bool) = json else { throw JsonReaderError("expected bool") }
+      guard case let .boolean(bool) = json else { throw JsonReaderError(expected: "bool", in: json) }
       return bool
     }
   }
   
-  static func dictionary(key: String) -> JsonReader<Json> {
+  static var dictionary: JsonReader<[String:Json]> {
     return JsonReader { json in
-      guard case let .object(dict) = json else { throw JsonReaderError("expected object") }
-      guard let x = dict[key] else { throw JsonReaderError("key: \(key) not found") }
+      guard case let .object(dict) = json else { throw JsonReaderError(expected: "dictionary", in: json) }
+      return dict
+    }
+  }
+  
+  static func dictionary(key: String) -> JsonReader<Json> {
+    return dictionary.map { dict in
+      guard let x = dict[key] else { throw JsonReaderError("key: \"\(key)\" not found in \(dict)") }
       return x
     }
   }
   
-  static func array(at index: Int) -> JsonReader<Json> {
+  static var array: JsonReader<[Json]> {
     return JsonReader { json in
-      guard case let .list(arr) = json else { throw JsonReaderError("expected array") }
-      guard arr.startIndex <= index && index < arr.endIndex else { throw JsonReaderError("index: \(index) out of bounds") }
+      guard case let .list(arr) = json else { throw JsonReaderError(expected: "array", in: json) }
+      return arr
+    }
+  }
+  
+  static func array(at index: Int) -> JsonReader<Json> {
+    return array.map { arr in
+      guard arr.startIndex <= index && index < arr.endIndex else { throw JsonReaderError("index: \(index) out of bounds in \(arr)") }
       return arr[index]
     }
   }
@@ -124,7 +141,7 @@ extension Json {
 }
 
 extension Json: Equatable {
-  static func == (lhs: Json, rhs: Json) -> Bool {
+  public static func == (lhs: Json, rhs: Json) -> Bool {
     switch (lhs, rhs) {
     case (.null, .null): return true
     case let (.text(s1), .text(s2)): return s1 == s2
@@ -137,25 +154,34 @@ extension Json: Equatable {
   }
 }
 
-private extension Dictionary {
-  func traverse<Mapped>(_ transform: (Value) throws -> Mapped?) rethrows -> [Key:Mapped]? {
-    var result = [Key:Mapped](minimumCapacity: count)
-    for (key, value) in self {
-      guard let newValue = try transform(value) else { return nil }
-      result[key] = newValue
+extension Json: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .null: return "null"
+    case let .text(s): return "\"\(s)\""
+    case let .number(n): return "\(n)"
+    case let .boolean(b): return "\(b)"
+    case let .object(dict): return "\(dict)"
+    case let .list(arr): return "\(arr)"
     }
-    return result
   }
 }
 
-private extension Array {
-  func traverse<Mapped>(_ transform: (Element) throws -> Mapped?) rethrows -> [Mapped]? {
-    var result: [Mapped] = []
-    result.reserveCapacity(count)
-    for element in self {
-      guard let newElement = try transform(element) else { return nil }
-      result.append(newElement)
-    }
-    return result
+public protocol JsonDecodable {
+  init(json: Json) throws
+  static var jsonReader: JsonReader<Self> { get }
+}
+
+public extension JsonDecodable {
+  init(json: Json) throws {
+    self = try Self.jsonReader.reading(from: json)
+  }
+  
+  static var jsonReader: JsonReader<Self> {
+    return JsonReader { json in try Self(json: json) }
+  }
+  
+  init(data: Data) throws {
+    try self.init(json: Json(data: data))
   }
 }
