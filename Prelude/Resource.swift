4fb92eb3860989a2265de5320645b4bfefd49056
Kristofer Hanes <kristoferhanes@gmail.com>
2017-11-30 04:28:02 -0800

    Changed import to specific structs and classes needed. Changed url: URL to request: URLRequest. Changed reader to a decode function. Added loadFromNetwork method. Added initializer for types that implement Decodable. Added private extension to URLSession adding a method that returns Async<Data>.

--- Prelude/Resource.swift
+++ Prelude/Resource.swift
@@ -2,30 +2,76 @@
 //  Resource.swift
 //  Prelude
 //
-//  Created by Kristofer Hanes on 6/15/17.
+//  Created by Kristofer Hanes on 2017-11-19.
 //  Copyright © 2017 Kristofer Hanes. All rights reserved.
 //
 
-import Foundation
+import struct Foundation.URLRequest
+import struct Foundation.URL
+import struct Foundation.Data
+import class  Foundation.JSONDecoder
+import class  Foundation.URLSession
 
-public struct Resource<Loaded> {
-  let url: URL
-  let reader: Reader<Data, Loaded>
+public struct Resource<Decoded> {
+  let request: URLRequest
+  let decode: (Data) throws -> Decoded
 }
 
 public extension Resource {
   
-  init(url: URL, reader: JsonReader<Loaded>) {
-    self.url = url
-    self.reader = reader.contramap(Json.init)
+  func loadFromNetwork() -> Async<Decoded> {
+    return URLSession.shared.data(request: request).map(decode)
+  }
+  
+}
+
+public extension Resource {
+  
+  init(url: URL, decode: @escaping (Data) throws -> Decoded) {
+    self.init(request: URLRequest(url: url), decode: decode)
   }
   
 }
 
 public extension Resource { // Functor
   
-  func map<Mapped>(_ transform: @escaping (Loaded) throws -> Mapped) -> Resource<Mapped> {
-    return Resource<Mapped>(url: url, reader: reader.map(transform))
+  func map<Mapped>(_ transform: @escaping (Decoded) throws -> Mapped) -> Resource<Mapped> {
+    return Resource<Mapped>(request: request, decode: { data in try transform(self.decode(data)) })
+  }
+  
+}
+
+public extension Resource where Decoded: Decodable {
+  
+  init(request: URLRequest) {
+    self.init(request: request) { data in try JSONDecoder().decode(Decoded.self, from: data) }
+  }
+  
+  init(url: URL) {
+    self.init(request: URLRequest(url: url))
+  }
+  
+}
+
+import Foundation
+
+private extension URLSession {
+  
+  func data(request: URLRequest) -> Async<Data> {
+    return Async { yield in
+      URLSession.shared.dataTask(with: request) { data, response, error in
+        switch (data, error) {
+        case let (data?, _):
+          yield(.ok(data))
+        case let (nil, error?):
+          yield(.error(error))
+        default:
+          guard let httpResponse = response as? HTTPURLResponse else { return }
+          let error = NSError(domain: "HTTP Error", code: httpResponse.statusCode, userInfo: httpResponse.allHeaderFields as? [String : Any])
+          yield(.error(error))
+        }
+      }.resume()
+    }
   }
   
 }
