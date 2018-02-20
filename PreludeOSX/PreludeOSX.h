e96a28215614f71d0b92160644823cbafffc2eff
Kristofer Hanes <kristoferhanes@gmail.com>
2018-02-19 17:36:29 -0800

    Initial

---
+++ PreludeOSX/PreludeOSX.h
@@ -0,0 +1,13 @@
+//
+//  PreludeOSX.h
+//  PreludeOSX
+//
+//  Created by Kristofer Hanes on 2018-02-13.
+//  Copyright © 2018 Kristofer Hanes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PreludeOSX : NSObject
+
+@end
---
+++ PreludeOSX/PreludeOSX.m
@@ -0,0 +1,13 @@
+//
+//  PreludeOSX.m
+//  PreludeOSX
+//
+//  Created by Kristofer Hanes on 2018-02-13.
+//  Copyright © 2018 Kristofer Hanes. All rights reserved.
+//
+
+#import "PreludeOSX.h"
+
+@implementation PreludeOSX
+
+@end
